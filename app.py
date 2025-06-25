8f1b2454726955ad241995f41508afe7fd2172a4
Mehmood ul Hassan <138776615+hassanshiekh19@users.noreply.github.com>
2025-06-25 21:09:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,9 @@ import cv2
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS to allow frontend to connect
+CORS(app)
 
-# Load trained model
-model = load_model('disease_classification_model.h5')  # Make sure the model is in the correct directory
-
-# Categories for classification (match this to your model's output)
+model = load_model('disease_classification_model.h5')
 CATEGORIES = ['Acne', 'Eczema', 'Psoriasis', 'Melanoma', 'BCC', 'Rosacea', 'Warts']
 
 @app.route('/')
@@ -21,20 +18,11 @@ def home():
 def predict():
     try:
         file = request.files['image']
-        
-        # Read image and decode it to OpenCV format
         img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
-
-        # Resize image to 128x128 (based on your training code)
         img = cv2.resize(img, (128, 128))
-
-        # Normalize pixel values
         img = img.astype('float32') / 255.0
-
-        # Expand dimensions for batch size
         img = np.expand_dims(img, axis=0)
 
-        # Make prediction
         prediction = model.predict(img)
         pred_index = np.argmax(prediction)
         predicted_label = CATEGORIES[pred_index]
